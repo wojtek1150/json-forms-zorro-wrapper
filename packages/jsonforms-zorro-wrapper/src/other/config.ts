1a8e3b6bbb0842864d98bd2efe96127037bf02d3
Wojciech <wojtek1150@gmail.com>
2024-01-22 09:49:52 +0100

    Update to v0.20.1
    * Allow extended types to be passed to the config

--- packages/jsonforms-zorro-wrapper/src/other/config.ts
+++ packages/jsonforms-zorro-wrapper/src/other/config.ts
@@ -1,13 +1,13 @@
-type MultiselectExternalDictionaryItem = { label: string; value: string; checked?: boolean } & Record<string, any>
-type MentionDictionaryItem = { avatar?: string; email: string; name: string } & Record<string, any>
+type MultiselectExternalDictionaryItem = { label: string; value: string; checked?: boolean } & Record<string, any>;
+type MentionDictionaryItem = { avatar?: string; email: string; name: string } & Record<string, any>;
 
 export interface Config {
   restrict?: boolean;
   trim?: boolean;
   showRequiredAsterisk?: boolean;
   hideColon?: boolean;
-  multiselectExternalDictionary?: MultiselectExternalDictionaryItem[];
-  mentionDictionary?: MentionDictionaryItem[];
+  multiselectExternalDictionary?: Record<string, MultiselectExternalDictionaryItem[]>;
+  mentionDictionary?: Record<string, MentionDictionaryItem[]>;
   disabledDateFn?: Record<string, (current: Date) => boolean>;
 
   [key: string]: any;
