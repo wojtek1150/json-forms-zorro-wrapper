0c0625f1d2d32a5d98142cf9b7227eb3185ca334
Wojciech <wojtek1150@gmail.com>
2024-01-29 13:40:05 +0100

    Update workspace to v16

--- packages/jsonforms-zorro-wrapper/src/jsonForms/jsonforms.module.ts
+++ packages/jsonforms-zorro-wrapper/src/jsonForms/jsonforms.module.ts
@@ -9,7 +9,6 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [CommonModule],
   declarations: [JsonFormsOutlet, UnknownRenderer, JsonForms, DescriptionRenderer],
-  entryComponents: [UnknownRenderer],
   exports: [JsonFormsOutlet, JsonForms, DescriptionRenderer],
 })
 export class JsonFormsModule {}
